737e033579cee5e6a951c6ef1c89470bb44b3d86
Conor O'Malley <comalley88@gmail.com>
2021-11-19 14:32:40 +0100

    schema updated

--- models/users.js
+++ models/users.js
@@ -1,12 +1,23 @@
 const mongoose = require('mongoose')
 
+const articleSchema = mongoose.Schema({
+    title: String,
+    content: String,
+    img: String,
+    language: String
+})
+
 const userSchema = mongoose.Schema({
     username: String,
     email: String,
     password: String,
     token: String,
+    lastlanguage: String,
+    wishlist: articleSchema
 })
 
 const userModel = mongoose.model('users', userSchema)
+const articleModel = mongoose.model('articles', articleSchema)
 
 module.exports = userModel
+module.exports = articleModel
