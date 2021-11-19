9de580fcb246e661b5d890823e2a291b9c7b2e07
Conor O'Malley <comalley88@gmail.com>
2021-11-19 14:29:10 +0100

    first commit

---
+++ models/connection.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+
+var options = {
+    connectTimeoutMS: 5000,
+    useUnifiedTopology : true,
+    useNewUrlParser: true,
+}
+
+mongoose.connect('mongodb+srv://Conor:fvz%23k%40jVWkcrgE9@cluster0.ftzjl.mongodb.net/morningnews?retryWrites=true&w=majority',
+    options,
+    function(err){
+        console.log(err);
+    }
+)
+
+module.exports = mongoose
